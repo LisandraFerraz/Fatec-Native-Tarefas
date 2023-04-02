750f1ecc6629b77d24ea6a046bc8e0ef0fec6636
Lisandra Ferraz de Santana <lisandraferraz.s@hotmail.com>
2023-04-02 19:55:04 -0300

    fix

--- native-atv-pratica-2/App.js
+++ native-atv-pratica-2/App.js
@@ -1,45 +1,57 @@
 import { StatusBar } from "expo-status-bar";
 import { useEffect, useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
-import Valores from "./src/valores";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { FlatList } from "react-native-web";
 
 export default function App() {
-  const [numberMin, setNumberMin] = useState();
-  const [numberMax, setNumberMax] = useState();
+  let numbers = [
+    {
+      key: 14,
+      number: 42,
+    },
+  ];
+
+  function getRandom(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  const Item = ({ title }) => (
+    <View style={styles.text}>
+      <Text style={styles.color}>O número aleatório é: {title}</Text>
+    </View>
+  );
 
   useEffect(() => {
-    var random1 = Math.floor(Math.random() * 5) + 1;
-    var random2 = Math.floor(Math.random() * 5) + 1;
-
-    if (random1 > random2) {
-      setNumberMax(random1);
-      setNumberMin(random2);
-    } else {
-      setNumberMax(random2);
-      setNumberMin(random1);
+    for (let i = 1; i < 10; i++) {
+      numbers.push({
+        number: getRandom(10, 100),
+        key: getRandom(10, 100),
+      });
     }
+    console.log(numbers);
   }, []);
 
   return (
-    <View style={styles.container}>
-      {numberMax === numberMin ? (
-        <Text>
-          Os valores {numberMax} e {numberMin} são os mesmos.
-        </Text>
-      ) : (
-        <Valores max={numberMax} min={numberMin} />
-      )}
-
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={numbers}
+        renderItem={({ item }) => <Item title={item.number} />}
+        keyExtractor={(item) => item.key}
+      />
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#17161c",
+    backgroundColor: "#f4f4f4",
     alignItems: "center",
     justifyContent: "center",
+  },
+  text: {
+    color: "#ffffff",
+    fontSize: 27,
   },
 });
